3b8933f283904b6f04939bc13ca2a1fa9432438b
Alejandro Mora <dambo29@mac.com>
2026-07-13 15:04:37 -0700

    Added I am a developer

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text("I am a developer!")
                 .font(.largeTitle)
                 .font(Font.largeTitle)
                 .fontWeight(.heavy)
